bf889c4fd3cadd75f987d8e665259455666d7898
Avinesh Pratap Singh <pratapavinesh2003@gmail.com>
2022-04-10 10:29:32 +0530

    committing files

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,27 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("                                             Welcome to our 64-bit kernel!\n");
+    print_str("                                             Developed by:\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("                                             -Avinesh Pratap Singh\n");
+    print_str("                                             -Rahul Agrawal\n");
+    print_str("                                             -Brijesh Kumar\n");
+    print_str("                                             -Ritik Gupta\n");
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+
+    print_str("                                             Mentored by:\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+
+    print_str("                                             -Ms. Diksha Rangwani\n");
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+
+    print_str("                                              Course Instructor:\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+
+    print_str("                                              -Dr. Hari Om\n");
+    print_set_color(PRINT_COLOR_LIGHT_CYAN, PRINT_COLOR_BLACK);
+    print_str("                                              IIT(ISM) DHANBAD");
+}
