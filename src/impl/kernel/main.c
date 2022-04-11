677de1ed82a602beb455841f5f4d7bcccfe4eb22
Avinesh Pratap Singh <pratapavinesh2003@gmail.com>
2022-04-11 12:39:22 +0530

    2

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,26 +2,28 @@
 
 void kernel_main() {
     print_clear();
+    print_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK);
+    print_str("\n\n\n\n\n\n\n");
+    print_str("                              Welcome to our 64-bit kernel!\n");
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    print_str("                                    Welcome to our 64-bit kernel!\n");
-    print_str("                                    Developed by:\n");
+    print_str("                                Developed by:\n");
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("                                    -Avinesh Pratap Singh\n");
     print_str("                                    -Rahul Agrawal\n");
     print_str("                                    -Brijesh Kumar\n");
-    print_str("                                     -Ritik Gupta\n");
+    print_str("                                    -Ritik Gupta\n");
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
 
-    print_str("                                     Mentored by:\n");
+    print_str("                                Mentored by:\n");
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
 
-    print_str("                                      -Ms. Diksha Rangwani\n");
+    print_str("                                    -Ms. Diksha Rangwani\n");
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
 
-    print_str("                                      Course Instructor:\n");
+    print_str("                                Course Instructor:\n");
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
 
-    print_str("                                      -Dr. Hari Om\n");
+    print_str("                                    -Dr. Hari Om\n\n");
     print_set_color(PRINT_COLOR_LIGHT_CYAN, PRINT_COLOR_BLACK);
-    print_str("                                       IIT(ISM) DHANBAD");
+    print_str("                                   IIT(ISM) DHANBAD");
 }
